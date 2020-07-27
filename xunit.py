0f9a445b344636ecf1cf3907758e8099756b2e2b
yabutayukinari <yabutayukinari@gmail.com>
2020-07-27 22:56:22 +0900

    21章 数え上げ ・P164まで実装

--- xunit.py
+++ xunit.py
@@ -13,6 +13,14 @@ class TestCase:
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return TestResult()
+
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
 
 
 class WasRun(TestCase):
@@ -32,5 +40,11 @@ class TestCaseTest(TestCase):
         test.run()
         assert ("setUp testMethod tearDown " == test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert ("1 run, 0 failed" == result.summary())
+
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
